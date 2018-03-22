31f99c3f6e9c09f90f2897b58d232afce1e63326
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-03-22 10:47:36 -0300

    Add files via upload

---
+++ Prueba 1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero1;
+    float numero2;
+
+    numero1 = 88;
+    numero2 = 33.2524;
+
+    printf("El numero entero es: %d y el numero flotante es: %.2f", numero1, numero2);
+
+    return 0;
+}
