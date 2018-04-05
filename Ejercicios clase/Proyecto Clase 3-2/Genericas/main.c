5d71767c6197b15f6718eda79cecdd17ade01689
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-04-05 12:12:10 -0300

    Add files via upload
    Funciones genéricas

--- Ejercicios clase/Proyecto Clase 3-2/Genericas/main.c
+++ Ejercicios clase/Proyecto Clase 3-2/Genericas/main.c
@@ -3,17 +3,19 @@
 
 int pedirEntero(char[], int, int);
 
+int validarEntero (int, int, int, char[]); //Recibe dato min y max
+
 int main()
 {
     int edad; //Entre 18-60
     int legajo; //Entre 1-1000
     int nota; //Entre 1-10
 
-    edad = pedirEntero("Ingrese edad: ", 18, 60);
+    edad = pedirEntero(" edad: ", 18, 60);
 
-    legajo = pedirEntero("Ingrese legajo: ", 1, 1000);
+    legajo = pedirEntero(" legajo: ", 1, 1000);
 
-    nota = pedirEntero("Ingrese nota: ", 1, 10);
+    nota = pedirEntero(" nota: ", 1, 10);
 
     printf("La edad es: %d y el legajo es: %d y la nota es: %d", edad, legajo, nota);
 
@@ -24,15 +26,22 @@ int pedirEntero(char mensaje[], int min, int max)
 {
     int numero;
 
-    printf("%s", mensaje);
+    printf("Ingrese %s", mensaje);
     scanf("%d", &numero);
 
-    while (numero<min || numero>max)
-    {
-        printf("Error: %s", mensaje);
-        scanf("%d",&numero);
-    }
+    numero = validarEntero(numero, min, max, mensaje);
+
 
     return numero;
 
+}
+
+int validarEntero(int dato, int min, int max, char mensaje[]) //el dato que entra es el dato que sale pero validado
+{
+    while (dato<min || dato>max)
+    {
+        printf("Error. Reingrese %s ", mensaje);
+        scanf("%d",&dato);
+    }
+    return dato;
 }
