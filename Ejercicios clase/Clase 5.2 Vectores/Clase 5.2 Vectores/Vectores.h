ddf7f3d5dbf8b91d6ca2115853d74cfcf599baf4
Lionel Herrero Battista <36281552+lionelherrerobattista@users.noreply.github.com>
2018-04-12 12:30:11 -0300

    Add files via upload

---
+++ Ejercicios clase/Clase 5.2 Vectores/Clase 5.2 Vectores/Vectores.h
@@ -0,0 +1,9 @@
+
+
+
+void mostrarVector (int[],int);
+void modificarValor (int[]);
+int buscarMaximo (int[], int);
+
+void cargarVector (int[], int);
+int buscarNumero (int[], int, int);
--- Ejercicios clase/Clase 5.2 Vectores/Clase 5.2 Vectores/main.c
+++ Ejercicios clase/Clase 5.2 Vectores/Clase 5.2 Vectores/main.c
@@ -1,62 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Vectores.h"
 #define TAM 5
 
 //Hacer un vector y que mostrar el valor máximo
+//Recibir vector, tamaño, numero
 
 int buscarMaximo (int[], int);
+
 
 int main()
 {
     int vector[TAM];
     int maximo;
+    int indice;
 
 
-
+    cargarVector(vector, TAM);
     maximo=buscarMaximo(vector,TAM);
 
-    /*for(i=0; i<TAM; i++)
-    {
-        printf("Ingrese un numero: ");
-        scanf("%d", &vector[i]);
-
-        if (flag==0 || vector[i]>maximo)
-        {
-            maximo=vector[i];
-            flag=1;
-        }
-        else
-        {
-            if (vector[i]>maximo)
-            {
-                maximo=vector[i];
-            }
-        }
-
-    }*/
 
     printf("El maximo es: %d", maximo);
+
+    indice= buscarNumero(vector, TAM, 14);
+    if (indice == -1)
+    {
+        printf("\nNo lo encontro!!!");
+    }
+    else
+    {
+        printf("\nEncontro el valor.");
+    }
 
 
     return 0;
 }
 
-int buscarMaximo (int vec[], int tam)
-{
-    int maximo;
-    int i;
-    int flag=0;
 
-    for(i=0; i<tam; i++)
-    {
-        printf("Ingrese un numero: ");
-        scanf("%d", &vec[i]);
-
-        if (flag==0 || vec[i]>maximo)
-        {
-            maximo=vec[i];
-            flag=1;
-        }
-    }
-    return maximo;
-}
