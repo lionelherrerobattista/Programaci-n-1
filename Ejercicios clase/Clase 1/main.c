c0e3b4cd872c07f2d56e773984cdde26e107d315
- <->
2018-06-12 18:34:25 -0300

    Clase 1

--- Ejercicios clase/Clase 1/main.c
+++ Ejercicios clase/Clase 1/main.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h> //Hacer el include para utilizar funcion getche()
 
 int main()
 {
-    int numero1;
-    float numero2;
+    //Pedir al usuario:
+    //-un numero entro
+    //-un numero con coma
+    //-un carácter
+    //-sumar los numeros
+    //-mostrar todo
 
-    char letra;
+    int numero1; //variable para guardar un entero
+    float numero2; //variable para guardar un real
 
-    float resultado;
+    char letra;//char guarda 1 SOLO carácter
 
-    printf("Escriba un numero entero: ");
-    scanf("%d", &numero1);
+    float resultado; //el resultado es un numero con coma
+
+    printf("Escriba un numero entero: ");//-imprime un mensaje por pantalla
+    scanf("%d", &numero1);//-lee nro. desde el teclado, agregar 1.máscara 2.dirección de memoria de la variable
 
     printf("Escriba un numero con coma: ");
     scanf("%f", &numero2);
 
     printf("Ingrese una letra: ");
-    letra = getche();
+    letra = getche();//Funcion que lee un caracter y lo guarda en una variable
+                    // y lo muestra en la consola (eco) diferencia con getch() no muestra nada.
 
-    printf("\nEl numero entero que ingreso es %d y el numero con coma %.2f", numero1, numero2);
+    printf("\nEl numero entero que ingreso es %d y el numero con coma %.2f", numero1, numero2);//%.2f muestra solo dos numeros después de la coma
 
-    resultado=(float) numero1+numero2;
+    resultado=(float) numero1+numero2;//castear el numero entero a flotante para no perder precisión decimal
     printf("\nEl resultado de la suma de los numeros es: %.2f",resultado);
 
     printf("\nLa letra que ingreso es: %c",letra);
